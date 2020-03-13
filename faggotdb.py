258722a2e3cb01f03801e8dfe675e260cce3bc42
l00tzOMG <52856431+l00tzOMG@users.noreply.github.com>
2020-03-13 20:38:14 +0100

    Update faggotdb.py

--- faggotdb.py
+++ faggotdb.py
@@ -50,9 +50,9 @@ def faggotdb(clip: vs.VideoNode, thrY=40, thrC=None, radiusY=12, radiusC=12, mas
 
     return core.std.MaskedMerge(filtered, clip, mask)
 
-def faggotdb_mod(clip: vs.VideoNode, thrY=40, thrC=None, radiusY=12, radiusC=12, mask="retinex", CbY=44, CrY=44, CbC=44, CrC=44, grainY=15, grainC=0, grainCC=0, sample_mode=2, dynamic_grainY=False, dynamic_grainC=False, tv_range=True, binarize=True, binarize_thr=5000) -> vs.VideoNode:
+def faggotdb_mod(clip: vs.VideoNode, thrY=40, thrC=None, radiusY=12, radiusC=12, mask="retinex", CbY=44, CrY=44, CbC=44, CrC=44, grainY=15, grainC=0, grainCC=0, sample_mode=2, dynamic_grainY=False, dynamic_grainC=False, tv_range=True, binarize=True, binarize_thr=5000, neo=False) -> vs.VideoNode:
 
-    # Original Idea: Author who created Fag3kdb. Edited by AlucardSama04; modded by l00t; added possibility to add chroma grain using grainCC variable
+    # Original Idea: Author who created Fag3kdb. Edited by AlucardSama04; modded by l00t; added possibility to add chroma grain using grainCC variable and to use neo_f3kdb (neo=True)
 
     if not isinstance(clip, vs.VideoNode):
         raise TypeError("FaggotDb: This is not a clip")
@@ -85,11 +85,16 @@ def faggotdb_mod(clip: vs.VideoNode, thrY=40, thrC=None, radiusY=12, radiusC=12,
         mask = core.std.Binarize(mask, threshold=binarize_thr)
 
     Y, U, V = split(clip)
-    U = core.f3kdb.Deband(U, range=radiusC, y=thrC, cb=CbC, cr=CrC, grainy=grainC, grainc=grainCC, sample_mode=sample_mode, dynamic_grain=dynamic_grainC, keep_tv_range=tv_range, output_depth=16)
-
-    V = core.f3kdb.Deband(V, range=radiusC, y=thrC, cb=CbC, cr=CrC, grainy=grainC, grainc=grainCC, sample_mode=sample_mode, dynamic_grain=dynamic_grainC, keep_tv_range=tv_range, output_depth=16)
-
-    filtered = core.std.ShufflePlanes([clip,U,V], [0,0,0], vs.YUV)
-    filtered = core.f3kdb.Deband(filtered, range=radiusY, y=thrY, cb=CbY, cr=CrY, grainy=grainY, grainc=grainCC, sample_mode=sample_mode, dynamic_grain=dynamic_grainY, keep_tv_range=tv_range, output_depth=16)
+    
+    if neo:
+        U = core.neo_f3kdb.Deband(U, range=radiusC, y=thrC, cb=CbC, cr=CrC, grainy=grainC, grainc=grainCC, sample_mode=sample_mode, dynamic_grain=dynamic_grainC, keep_tv_range=tv_range, output_depth=16)
+        V = core.neo_f3kdb.Deband(V, range=radiusC, y=thrC, cb=CbC, cr=CrC, grainy=grainC, grainc=grainCC, sample_mode=sample_mode, dynamic_grain=dynamic_grainC, keep_tv_range=tv_range, output_depth=16)
+        filtered = core.std.ShufflePlanes([clip,U,V], [0,0,0], vs.YUV)
+        filtered = core.neo_f3kdb.Deband(filtered, range=radiusY, y=thrY, cb=CbY, cr=CrY, grainy=grainY, grainc=grainCC, sample_mode=sample_mode, dynamic_grain=dynamic_grainY, keep_tv_range=tv_range, output_depth=16)
+    else:
+        U = core.f3kdb.Deband(U, range=radiusC, y=thrC, cb=CbC, cr=CrC, grainy=grainC, grainc=grainCC, sample_mode=sample_mode, dynamic_grain=dynamic_grainC, keep_tv_range=tv_range, output_depth=16)
+        V = core.f3kdb.Deband(V, range=radiusC, y=thrC, cb=CbC, cr=CrC, grainy=grainC, grainc=grainCC, sample_mode=sample_mode, dynamic_grain=dynamic_grainC, keep_tv_range=tv_range, output_depth=16)
+        filtered = core.std.ShufflePlanes([clip,U,V], [0,0,0], vs.YUV)
+        filtered = core.f3kdb.Deband(filtered, range=radiusY, y=thrY, cb=CbY, cr=CrY, grainy=grainY, grainc=grainCC, sample_mode=sample_mode, dynamic_grain=dynamic_grainY, keep_tv_range=tv_range, output_depth=16)
 
     return core.std.MaskedMerge(filtered, clip, mask)
