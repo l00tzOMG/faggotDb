e02f5dcc81a2548e52cdd8ac77c354242747cd58
l00tzOMG <52856431+l00tzOMG@users.noreply.github.com>
2020-05-22 11:52:57 +0200

    update
    updated underlying FaggotDB + added back nasty features :)
    - possibility to deband chroma channels twice
    - do masking internally, like in old version
    - added option to use a simply binary mask or grayscale mask
    - disable bit upsampling by default; if true do it automatically
    - added option to blur edges of edgemasks

--- faggotdb_mod.py
+++ faggotdb_mod.py
@@ -1,20 +1,25 @@
-from vapoursynth import core, VideoNode, GRAY, YUV
+#from vapoursynth import core, VideoNode, GRAY, YUV
 import vapoursynth as vs
+import fvsfunc as fvf # https://github.com/Irrational-Encoding-Wizardry/fvsfunc/blob/master/fvsfunc.py
+from vsutil import plane # https://github.com/Irrational-Encoding-Wizardry/vsutil/blob/master/vsutil.py
 import kagefunc as kgf #https://github.com/Irrational-Encoding-Wizardry/kagefunc/
-import fvsfunc as fvf #https://github.com/Irrational-Encoding-Wizardry/fvsfunc/
-from vsutil import split #https://github.com/Irrational-Encoding-Wizardry/vsutil/blob/master/vsutil.py#L113
 
 core = vs.core
 
-def faggotdb(clip: vs.VideoNode, thrY=40, thrC=None, radiusY=12, radiusC=12, mask="retinex", CbY=44, CrY=44, CbC=44, CrC=44, grainY=15, grainC=0, sample_mode=2, dynamic_grainY=False, dynamic_grainC=False, tv_range=True, binarize=True, binarize_thr=5000) -> vs.VideoNode:
+def FaggotDB(clip: vs.VideoNode, thrY=40, thrC=None, radiusY=15, radiusC=15, CbY=44, CrY=44, CbC=44, CrC=44, grainY=20, grainC=None, sample_mode=2, neo=False, dynamic_grainY=False, dynamic_grainC=False, tv_range=True, mask=None) -> vs.VideoNode:
+
+    funcName = "FaggotDB"
 
     # Original Idea: Author who created Fag3kdb. Edited by AlucardSama04
 
     if not isinstance(clip, vs.VideoNode):
-        raise TypeError("FaggotDb: This is not a clip")
+        raise TypeError(f"{funcName}: This is not a clip")
 
     if clip.format.bits_per_sample != 16:
-        raise TypeError("FaggotDb: Only 16Bit clips are supported")
+        raise TypeError(f"{funcName}: Only 16Bit clips are supported")
+
+    if not isinstance(mask, vs.VideoNode):
+        raise vs.Error(f"{funcName}: mask' only clip inputs")
 
     if thrC is None:
         thrC = int(round(thrY / 2))
@@ -22,52 +27,43 @@ def faggotdb(clip: vs.VideoNode, thrY=40, thrC=None, radiusY=12, radiusC=12, mas
     if grainC is None:
         grainC = int(round(grainY / 2))
 
-    if mask in [-1]:
-        mask = clip
-    elif mask in [0, "retinex"]:
-        mask = kgf.retinex_edgemask(clip)
-    elif mask in [1, "kirsch"]:
-        mask = kgf.kirsch(clip)
-    elif mask in [2, "Sobel"]:
-        mask = core.std.Sobel(clip, scale=1)
-    elif mask in [3, "Prewitt"]:
-        mask = core.std.Prewitt(clip)
-    elif mask in [4, "GF"]:
-        mask = fvf.GradFun3(clip, mask=2, debug=1)
-    else:
-         raise ValueError("Unknown Mask Mode")
+    f3kdb = core.neo_f3kdb.Deband if neo else core.f3kdb.Deband
 
-    if binarize:
-        mask = core.std.Binarize(mask, threshold=binarize_thr)
+    U = plane(clip, 1)
+    V = plane(clip, 2)
+    U = f3kdb(U, range=radiusC, y=thrC, cb=CbC, cr=CrC, grainy=grainC, grainc=0, sample_mode=sample_mode, dynamic_grain=dynamic_grainC, keep_tv_range=tv_range, output_depth=16)
 
-    Y, U, V = split(clip)
-    U = core.f3kdb.Deband(U, range=radiusC, y=thrC, cb=CbC, cr=CrC, grainy=grainC, grainc=0, sample_mode=sample_mode, dynamic_grain=dynamic_grainC, keep_tv_range=tv_range, output_depth=16)
-
-    V = core.f3kdb.Deband(V, range=radiusC, y=thrC, cb=CbC, cr=CrC, grainy=grainC, grainc=0, sample_mode=sample_mode, dynamic_grain=dynamic_grainC, keep_tv_range=tv_range, output_depth=16)
+    V = f3kdb(V, range=radiusC, y=thrC, cb=CbC, cr=CrC, grainy=grainC, grainc=0, sample_mode=sample_mode, dynamic_grain=dynamic_grainC, keep_tv_range=tv_range, output_depth=16)
 
     filtered = core.std.ShufflePlanes([clip,U,V], [0,0,0], vs.YUV)
-    filtered = core.f3kdb.Deband(filtered, range=radiusY, y=thrY, cb=CbY, cr=CrY, grainy=grainY, grainc=0, sample_mode=sample_mode, dynamic_grain=dynamic_grainY, keep_tv_range=tv_range, output_depth=16)
+    filtered = f3kdb(filtered, range=radiusY, y=thrY, cb=CbY, cr=CrY, grainy=grainY, grainc=0, sample_mode=sample_mode, dynamic_grain=dynamic_grainY, keep_tv_range=tv_range, output_depth=16)
 
-    return core.std.MaskedMerge(filtered, clip, mask)
+    return core.std.MaskedMerge(filtered, clip, mask)#from vapoursynth import core, VideoNode, GRAY, YUV
 
-def faggotdb_mod(clip: vs.VideoNode, thrY=40, thrC=None, radiusY=12, radiusC=12, mask="retinex", CbY=44, CrY=44, CbC=44, CrC=44, grainY=15, grainC=0, grainCC=0, sample_mode=2, dynamic_grainY=False, dynamic_grainC=False, tv_range=True, binarize=True, binarize_thr=5000, neo=False) -> vs.VideoNode:
+def faggotdb_mod(clip: vs.VideoNode, thrY=40, thrC=None, radiusY=15, radiusC=15, CbY=44, CrY=44, CbC=44, CrC=44, grainY=32, grainC=None, grainCC=0, sample_mode=2, neo=True, dynamic_grainY=False, dynamic_grainC=False, tv_range=True, mask="retinex", binarize=False, binarize_thr=70, grayscale=True, bitresamp=False, outbits=None, blurmask=True, horizblur=2, vertblur=2) -> vs.VideoNode:
 
-    # Original Idea: Author who created Fag3kdb. Edited by AlucardSama04; modded by l00t; added possibility to add chroma grain using grainCC variable and to use neo_f3kdb (neo=True)
+    funcName = "faggotdb_mod" # name kept to be fallback compatible
+
+    # Original Idea: Author who created Fag3kdb. Edited by AlucardSama04; additional modifications by l00t
 
     if not isinstance(clip, vs.VideoNode):
-        raise TypeError("FaggotDb: This is not a clip")
+        raise TypeError(f"{funcName}: This is not a clip")
+        
+    if outbits is None:
+        outbits = clip.format.bits_per_sample
 
-    if clip.format.bits_per_sample != 16:
-        clip = fvf.Depth(clip, bits=16) # instead of error, auto convert to 16 bits
+    if bitresamp:
+        if clip.format.bits_per_sample != outbits:
+            clip = fvf.Depth(clip, bits=outbits) # instead of error, auto convert to 16 bits
+    elif clip.format.bits_per_sample != outbits:
+        raise TypeError(f"{funcName}: Input-output bitdepth mismatch")
 
-    if thrC is None:
-        thrC = int(round(thrY / 2))
-
-    if grainC is None:
-        grainC = int(round(grainY / 2))
-
-    if mask in [-1]:
+    # if not isinstance(mask, vs.VideoNode):
+        # raise vs.Error(f"{funcName}: mask' only clip inputs")
+        
+    if mask in [-1]: # more user friendly if we do the masking intentionally
         mask = clip
+        blurmask = False
     elif mask in [0, "retinex"]:
         mask = kgf.retinex_edgemask(clip)
     elif mask in [1, "kirsch"]:
@@ -79,22 +75,37 @@ def faggotdb_mod(clip: vs.VideoNode, thrY=40, thrC=None, radiusY=12, radiusC=12,
     elif mask in [4, "GF"]:
         mask = fvf.GradFun3(clip, mask=2, debug=1)
     else:
-         raise ValueError("Unknown Mask Mode")
+         raise ValueError(f"{funcName}: Unknown Mask Mode")
 
-    if binarize:
+    if grayscale:
+        mask = core.std.ShufflePlanes(mask, planes=0, colorfamily=vs.GRAY)
+         
+    if binarize:     # binarize treshold should be adjusted according to bitdepth
         mask = core.std.Binarize(mask, threshold=binarize_thr)
+        
+    if blurmask:
+        mask = core.std.BoxBlur(mask, hradius=horizblur, vradius=vertblur)
 
-    Y, U, V = split(clip)
+    if thrC is None:
+        thrC = int(round(thrY / 2))
+
+    if grainC is None:
+        grainC = int(round(grainY / 2))
+
+    if grainCC is None:
+        grainCC = 0
+        
+    f3kdb = core.neo_f3kdb.Deband if neo else core.f3kdb.Deband
+
+    U = plane(clip, 1)
+    V = plane(clip, 2)
     
-    if neo:
-        U = core.neo_f3kdb.Deband(U, range=radiusC, y=thrC, cb=CbC, cr=CrC, grainy=grainC, grainc=grainCC, sample_mode=sample_mode, dynamic_grain=dynamic_grainC, keep_tv_range=tv_range, output_depth=16)
-        V = core.neo_f3kdb.Deband(V, range=radiusC, y=thrC, cb=CbC, cr=CrC, grainy=grainC, grainc=grainCC, sample_mode=sample_mode, dynamic_grain=dynamic_grainC, keep_tv_range=tv_range, output_depth=16)
-        filtered = core.std.ShufflePlanes([clip,U,V], [0,0,0], vs.YUV)
-        filtered = core.neo_f3kdb.Deband(filtered, range=radiusY, y=thrY, cb=CbY, cr=CrY, grainy=grainY, grainc=grainCC, sample_mode=sample_mode, dynamic_grain=dynamic_grainY, keep_tv_range=tv_range, output_depth=16)
-    else:
-        U = core.f3kdb.Deband(U, range=radiusC, y=thrC, cb=CbC, cr=CrC, grainy=grainC, grainc=grainCC, sample_mode=sample_mode, dynamic_grain=dynamic_grainC, keep_tv_range=tv_range, output_depth=16)
-        V = core.f3kdb.Deband(V, range=radiusC, y=thrC, cb=CbC, cr=CrC, grainy=grainC, grainc=grainCC, sample_mode=sample_mode, dynamic_grain=dynamic_grainC, keep_tv_range=tv_range, output_depth=16)
-        filtered = core.std.ShufflePlanes([clip,U,V], [0,0,0], vs.YUV)
-        filtered = core.f3kdb.Deband(filtered, range=radiusY, y=thrY, cb=CbY, cr=CrY, grainy=grainY, grainc=grainCC, sample_mode=sample_mode, dynamic_grain=dynamic_grainY, keep_tv_range=tv_range, output_depth=16)
+    U = f3kdb(U, range=radiusC, y=thrC, cb=CbC, cr=CrC, grainy=grainC, grainc=0, sample_mode=sample_mode, dynamic_grain=dynamic_grainC, keep_tv_range=tv_range, output_depth=outbits)
+    
+    V = f3kdb(V, range=radiusC, y=thrC, cb=CbC, cr=CrC, grainy=grainC, grainc=0, sample_mode=sample_mode, dynamic_grain=dynamic_grainC, keep_tv_range=tv_range, output_depth=outbits)
+
+    filtered = core.std.ShufflePlanes([clip,U,V], [0,0,0], vs.YUV)
+    
+    filtered = f3kdb(filtered, range=radiusY, y=thrY, cb=CbY, cr=CrY, grainy=grainY, grainc=grainCC, sample_mode=sample_mode, dynamic_grain=dynamic_grainY, keep_tv_range=tv_range, output_depth=outbits) # if grainCC > 0 UV planes will be debanded once again
 
     return core.std.MaskedMerge(filtered, clip, mask)
